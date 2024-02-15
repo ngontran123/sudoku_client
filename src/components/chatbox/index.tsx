ebfb2fb6af16f7ecb82a8042069044599978a239
quân huỳnh <huynhkiengquan@gmail.com>
2024-02-15 16:05:25 +0700

    Add Daily Update.

--- src/components/chatbox/index.tsx
+++ src/components/chatbox/index.tsx
@@ -8,8 +8,8 @@ import {
   MDBCardFooter,
   MDBIcon,
 } from "mdb-react-ui-kit";
-//import "@fortawesome/fontawesome-free/css/all.min.css";
-//import "bootstrap-css-only/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
 import gameServices from "../../services/gameServices";
 import socketService from "../../services/socketService";
 import cloneDeep from "lodash.clonedeep";
