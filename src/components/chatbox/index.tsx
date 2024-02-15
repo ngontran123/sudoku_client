632047870ca32d32256cd98bc76335a1247dc9e0
quân huỳnh <huynhkiengquan@gmail.com>
2024-02-15 15:49:21 +0700

    Add Daily Update.

--- src/components/chatbox/index.tsx
+++ src/components/chatbox/index.tsx
@@ -9,7 +9,7 @@ import {
   MDBIcon,
 } from "mdb-react-ui-kit";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import "bootstrap-css-only/css/bootstrap.min.css";
+//import "bootstrap-css-only/css/bootstrap.min.css";
 import gameServices from "../../services/gameServices";
 import socketService from "../../services/socketService";
 import cloneDeep from "lodash.clonedeep";
